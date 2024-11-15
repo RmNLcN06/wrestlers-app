5e505ed445e62bcc4a9074c21bc17ae2f118c5f6
RmNLcN06 <romain.luciano@orange.fr>
2024-11-15 22:28:53 +0100

    Add properties to Wrestler interface

--- src/app/wrestler.ts
+++ src/app/wrestler.ts
@@ -1,2 +1,14 @@
 export interface Wrestler {
+    id: number;
+    firstname: string;
+    lastname: string;
+    alias: string;
+    age: number;
+    state: string;
+    city: string;
+    photo: string;
+    retire: boolean;
+    injury: boolean;
+    federation: string;
+    show: string;
 }
