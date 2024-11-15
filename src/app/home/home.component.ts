73c480b2c170d38df1cb72a972ccae2593b57778
RmNLcN06 <romain.luciano@orange.fr>
2024-11-15 21:50:40 +0100

    Add new component to the app's layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WrestlerComponent } from '../wrestler/wrestler.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [WrestlerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
