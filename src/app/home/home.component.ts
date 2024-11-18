18a97c81894ef483a30ec08ac5f21f77648336f4
RmNLcN06 <romain.luciano@orange.fr>
2024-11-18 09:35:56 +0100

    Create a test wrestler for the app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WrestlerComponent } from '../wrestler/wrestler.component';
+import { Wrestler } from '../wrestler';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,20 @@ import { WrestlerComponent } from '../wrestler/wrestler.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'assets'
 
+  wrestler: Wrestler = {
+    id: 1,
+    firstname: 'John',
+    lastname: 'Cena',
+    alias: 'The Champ',
+    age: 47,
+    state: 'Massachusetts',
+    city: 'West Newbury',
+    photo: `${this.baseUrl}/john_cena.jpg`,
+    retire: false,
+    injury: false,
+    federation: 'WWE',
+    show: "Raw",
+  };
 }
