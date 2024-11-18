48a1c07756e7d301c349e64fb6095130e10de7c2
RmNLcN06 <romain.luciano@orange.fr>
2024-11-18 15:45:25 +0100

    Add wrestlers data to HomeComponent & Update HomeComponent template with ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,102 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { WrestlerComponent } from '../wrestler/wrestler.component';
 import { Wrestler } from '../wrestler';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WrestlerComponent],
+  imports: [CommonModule, WrestlerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   readonly baseUrl = 'assets'
 
-  wrestler: Wrestler = {
-    id: 1,
-    firstname: 'John',
-    lastname: 'Cena',
-    alias: 'The Champ',
-    age: 47,
-    state: 'Massachusetts',
-    city: 'West Newbury',
-    photo: `${this.baseUrl}/john_cena.jpg`,
-    retire: false,
-    injury: false,
-    federation: 'WWE',
-    show: "Raw",
-  };
+  wrestlerList: Wrestler[] = [
+    {
+      id: 0,
+      firstname: 'John',
+      lastname: 'Cena',
+      alias: 'The Champ',
+      age: 47,
+      state: 'Massachusetts',
+      city: 'West Newbury',
+      photo: `${this.baseUrl}/john_cena.jpg`,
+      retire: false,
+      injury: false,
+      federation: 'WWE',
+      show: "Raw",
+    },
+    {
+      id: 1,
+      firstname: 'Mark',
+      lastname: 'Calaway',
+      alias: 'The Undertaker',
+      age: 59,
+      state: 'Texas',
+      city: 'Houston',
+      photo: `${this.baseUrl}/the_undertaker.jpg`,
+      retire: true,
+      injury: false,
+      federation: 'WWE',
+      show: "Smackdown",
+    },
+    {
+      id: 2,
+      firstname: 'Randy',
+      lastname: 'Orton',
+      alias: 'The Viper',
+      age: 44,
+      state: 'Tennessee',
+      city: 'Knoxville',
+      photo: `${this.baseUrl}/randy_orton.jpg`,
+      retire: false,
+      injury: false,
+      federation: 'WWE',
+      show: "Raw",
+    },
+    {
+      id: 3,
+      firstname: 'Dave',
+      lastname: 'Bautista',
+      alias: 'The Animal',
+      age: 55,
+      state: 'District of Colombia',
+      city: 'Washington',
+      photo: `${this.baseUrl}/dave_bautista.jpg`,
+      retire: true,
+      injury: false,
+      federation: 'WWE',
+      show: "Smackdown",
+    },
+    {
+      id: 4,
+      firstname: 'Will',
+      lastname: 'Ospreay',
+      alias: 'The Aerial Assassin',
+      age: 31,
+      state: 'London',
+      city: 'Rainham',
+      photo: `${this.baseUrl}/will_ospreay.jpg`,
+      retire: false,
+      injury: false,
+      federation: 'AEW',
+      show: "Dynamite",
+    },
+    {
+      id: 5,
+      firstname: 'Trevor',
+      lastname: 'Mann',
+      alias: 'Ricochet',
+      age: 36,
+      state: 'Illinois',
+      city: 'Alton',
+      photo: `${this.baseUrl}/ricochet.jpg`,
+      retire: false,
+      injury: false,
+      federation: 'AEW',
+      show: "Dynamite",
+    },
+  ];
 }
