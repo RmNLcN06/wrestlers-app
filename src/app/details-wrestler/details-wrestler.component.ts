3c4f88075789f80dcb1d412db8f6be0adfdba316
RmNLcN06 <romain.luciano@orange.fr>
2024-11-19 22:48:44 +0100

    Get route parameters

--- src/app/details-wrestler/details-wrestler.component.ts
+++ src/app/details-wrestler/details-wrestler.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { WrestlerService } from '../wrestler.service';
+import { Wrestler } from '../wrestler';
 
 @Component({
   selector: 'app-details-wrestler',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details-wrestler.component.html',
   styleUrl: './details-wrestler.component.css'
 })
 export class DetailsWrestlerComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  wrestlerId = -1;
+  
+  constructor() {
+    this.wrestlerId = Number(this.route.snapshot.params['id']);
+  }
 }
