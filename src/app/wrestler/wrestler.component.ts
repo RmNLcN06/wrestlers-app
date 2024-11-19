1caf5937678fd5f19f03f1efb6413b4a5b19df52
RmNLcN06 <romain.luciano@orange.fr>
2024-11-19 22:06:23 +0100

    Using routerLink for dynamic navigation

--- src/app/wrestler/wrestler.component.ts
+++ src/app/wrestler/wrestler.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Wrestler } from '../wrestler';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-wrestler',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './wrestler.component.html',
   styleUrl: './wrestler.component.css'
 })
