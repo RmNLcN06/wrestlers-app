1cd2fe7fee9b89b3947ffda01058a3bcf070f89f
RmNLcN06 <romain.luciano@orange.fr>
2024-11-22 14:20:19 +0100

    Update service to use web server instead of local array => GET request over HTTP

--- src/app/wrestler.service.ts
+++ src/app/wrestler.service.ts
@@ -6,101 +6,17 @@ import { Wrestler } from './wrestler';
 })
 export class WrestlerService {
 
-  readonly baseUrl = 'assets'
+  // readonly baseUrl = 'assets'
+  url = 'http://localhost:3000/wrestlers';
 
-  wrestlerList: Wrestler[] = [
-    {
-      id: 0,
-      firstname: 'John',
-      lastname: 'Cena',
-      alias: 'The Champ',
-      age: 47,
-      state: 'Massachusetts',
-      city: 'West Newbury',
-      photo: `${this.baseUrl}/john_cena.jpg`,
-      retire: false,
-      injury: false,
-      federation: 'WWE',
-      show: "Raw",
-    },
-    {
-      id: 1,
-      firstname: 'Mark',
-      lastname: 'Calaway',
-      alias: 'The Undertaker',
-      age: 59,
-      state: 'Texas',
-      city: 'Houston',
-      photo: `${this.baseUrl}/the_undertaker.jpg`,
-      retire: true,
-      injury: false,
-      federation: 'WWE',
-      show: "Smackdown",
-    },
-    {
-      id: 2,
-      firstname: 'Randy',
-      lastname: 'Orton',
-      alias: 'The Viper',
-      age: 44,
-      state: 'Tennessee',
-      city: 'Knoxville',
-      photo: `${this.baseUrl}/randy_orton.jpg`,
-      retire: false,
-      injury: false,
-      federation: 'WWE',
-      show: "Raw",
-    },
-    {
-      id: 3,
-      firstname: 'Dave',
-      lastname: 'Bautista',
-      alias: 'The Animal',
-      age: 55,
-      state: 'District of Colombia',
-      city: 'Washington',
-      photo: `${this.baseUrl}/dave_bautista.jpg`,
-      retire: true,
-      injury: false,
-      federation: 'WWE',
-      show: "Smackdown",
-    },
-    {
-      id: 4,
-      firstname: 'Will',
-      lastname: 'Ospreay',
-      alias: 'The Aerial Assassin',
-      age: 31,
-      state: 'London',
-      city: 'Rainham',
-      photo: `${this.baseUrl}/will_ospreay.jpg`,
-      retire: false,
-      injury: false,
-      federation: 'AEW',
-      show: "Dynamite",
-    },
-    {
-      id: 5,
-      firstname: 'Trevor',
-      lastname: 'Mann',
-      alias: 'Ricochet',
-      age: 36,
-      state: 'Illinois',
-      city: 'Alton',
-      photo: `${this.baseUrl}/ricochet.jpg`,
-      retire: false,
-      injury: false,
-      federation: 'AEW',
-      show: "Dynamite",
-    },
-  ];
-
-  getAllWrestlers(): Wrestler[] {
-    return this.wrestlerList;
+  async getAllWrestlers(): Promise<Wrestler[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? [];
   }
 
-  getWrestlerById(id: number): Wrestler | undefined {
-    return this.wrestlerList.find((wrestler) => wrestler.id === id);
+  async getWrestlerById(id: number): Promise<Wrestler | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? [];
   }
 
   constructor() { }
